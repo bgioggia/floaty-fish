53a07202256f2733d291d6279048b34b853170d5
bgioggia <bgioggia@gmail.com>
2021-10-08 18:15:11 -0400

    Fish despawns when colliding with top or bottom border of gameWindow

--- src/fish.js
+++ src/fish.js
@@ -7,36 +7,47 @@ export default class Fish extends React.Component {
         this.state = {
             yPos: 500,
             yVelocity: 0,
-            delta: 0.5};
+            delta: 0.5,
+            alive: true};
     }
 
     onKeyPress = (event) => {
-        if (event.keyCode === 32) {  //keycode for spacebar
+        if(this.state.alive){
+            if (event.keyCode === 32) {  //keycode for spacebar
+                this.setState((state) => {
+                    return {yVelocity: state.yVelocity + state.delta*15};
+                });
+                if (this.state.yVelocity > 15){ //Keeps the fish relatively bouncy
+                    this.setState(() => {
+                        return {yVelocity: 15};
+                    });
+                }
+                this.setState((state) => {
+                    return {yPos: state.yPos + state.yVelocity};
+                });}
+        }
+    }
+
+    tick() {
+        if(this.state.alive) {
             this.setState((state) => {
-                return {yVelocity: state.yVelocity + state.delta*15};
+                return {yVelocity: state.yVelocity - state.delta};
             });
-            if (this.state.yVelocity > 15){ //Keeps the fish relatively bouncy
+            if (this.state.yVelocity < -10) { //Keeps the fish relatively bouncy
                 this.setState(() => {
-                    return {yVelocity: 15};
+                    return {yVelocity: -10};
                 });
             }
             this.setState((state) => {
                 return {yPos: state.yPos + state.yVelocity};
-            });}
-    }
-
-    tick() {
-        this.setState((state) => {
-            return {yVelocity: state.yVelocity - state.delta};
-        });
-        if (this.state.yVelocity < -10){ //Keeps the fish relatively bouncy
-            this.setState(() => {
-                return {yVelocity: -10};
             });
         }
-        this.setState((state) => {
-            return {yPos: state.yPos + state.yVelocity};
-        });
+    }
+
+    die() {
+        this.setState(() => {
+            return {alive: false}
+        })
     }
 
     componentDidMount(){
@@ -49,16 +60,17 @@ export default class Fish extends React.Component {
     }
 
     render() {
+        const alive = this.state.alive;
         return (
-            <div id='player_fish'
+            <div id={alive ? 'player_fish': null}
                  style={{
                      position: "absolute",
                      marginTop: `${this.state.yPos}px`,
                  }}>
 
-                <div id='player_tail'/>
-                <div id='player_body'><
-                    div id='player_eye'/>
+                <div id={alive ? 'player_tail' : null}/>
+                <div id={alive ? 'player_body' : null}>
+                    <div id={alive ? 'player_eye' : null}/>
                 </div>
 
             </div>
--- src/gameWindow.js
+++ src/gameWindow.js
@@ -5,17 +5,13 @@ export default class gameWindow extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = {
-            yPos: 0,
-            yVelocity: 0,
-            delta: 0.5
-        };
         this.fish= React.createRef();
     }
 
     getFishPos(){
-        console.log(this.fish.current.state.yPos)
-        return this.fish.current.state.yPos
+        if(this.fish.current.state.yPos < -20 || this.fish.current.state.yPos > 623) {
+            this.fish.current.die();
+        }
     }
 
     componentDidMount(){
