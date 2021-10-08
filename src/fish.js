b0a828526171efc6f3a7342546fa412c514049ff
bgioggia <bgioggia@gmail.com>
2021-10-08 19:25:26 -0400

    Added comments to fish.js functions and added more robust handling of dead fish

--- src/fish.js
+++ src/fish.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './playerFish.css';
 export default class Fish extends React.Component {
 
+    // Constructor for Fish
     constructor(props) {
         super(props);
         this.state = {
@@ -11,6 +12,9 @@ export default class Fish extends React.Component {
             alive: true};
     }
 
+    // Handles fish interactions with keyboard button presses
+    // Spacebar -> Fish will increase its downard velocity by 15 pixels/s with a cap of 15
+    // If the fish is dead this function does nothing.
     onKeyPress = (event) => {
         if(this.state.alive){
             if (event.keyCode === 32) {  //keycode for spacebar
@@ -28,6 +32,9 @@ export default class Fish extends React.Component {
         }
     }
 
+    // tick function handles runs every .05 seconds
+    // Causes fish to increase its upward velocity by 10 every .05 seconds with a cap of -10
+    // If the fish is dead, this function puts the fish at the starting postion and speed to play again.
     tick() {
         if(this.state.alive) {
             this.setState((state) => {
@@ -42,23 +49,36 @@ export default class Fish extends React.Component {
                 return {yPos: state.yPos + state.yVelocity};
             });
         }
+        else {
+            this.setState(() => {
+                return {yVelocity: 0, yPos: 500};
+            })
+        }
     }
 
+    // The die function kills the fish.
+    // Sets alive to false which is handled in other functions
     die() {
         this.setState(() => {
             return {alive: false}
         })
     }
 
+    // ComponentDidMount()
+    // runs the tick function every 0.05 seconds.
+    // enables keydown function
     componentDidMount(){
         this.interval = setInterval(() => this.tick(), 50);
         document.addEventListener("keydown", this.onKeyPress, false);
     }
+
+    // Clears
     componentWillUnmount(){
         clearInterval(this.interval);
         document.removeEventListener("keydown", this.onKeyPress, false);
     }
 
+    // Renders fish if the fish is alive. If the fish is dead, nothing is rendered.
     render() {
         const alive = this.state.alive;
         return (
