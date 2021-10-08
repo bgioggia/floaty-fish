22c02bcdc4150af05006ee4719c42fbaed11274e
bgioggia <bgioggia@gmail.com>
2021-10-07 22:09:03 -0400

    Fish starts at bottom of screen, and slightly tweaked values to make it float slower

--- src/fish.js
+++ src/fish.js
@@ -5,7 +5,7 @@ export default class Fish extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            yPos: 0,
+            yPos: 500,
             yVelocity: 0,
             delta: 0.5};
     }
@@ -13,7 +13,7 @@ export default class Fish extends React.Component {
     onKeyPress = (event) => {
         if (event.keyCode === 32) {  //keycode for spacebar
             this.setState((state) => {
-                return {yVelocity: state.yVelocity + state.delta*20};
+                return {yVelocity: state.yVelocity + state.delta*15};
             });
             if (this.state.yVelocity > 15){ //Keeps the fish relatively bouncy
                 this.setState(() => {
@@ -29,9 +29,9 @@ export default class Fish extends React.Component {
         this.setState((state) => {
             return {yVelocity: state.yVelocity - state.delta};
         });
-        if (this.state.yVelocity < -15){ //Keeps the fish relatively bouncy
+        if (this.state.yVelocity < -10){ //Keeps the fish relatively bouncy
             this.setState(() => {
-                return {yVelocity: -15};
+                return {yVelocity: -10};
             });
         }
         this.setState((state) => {
@@ -57,7 +57,9 @@ export default class Fish extends React.Component {
                  }}>
 
                 <div id='player_tail'/>
-                <div id='player_body'><div id='player_eye'/></div>
+                <div id='player_body'><
+                    div id='player_eye'/>
+                </div>
 
             </div>
         );
