aca8e1112d1273f876c0cf700996bda51bf2e38f
bgioggia <bgioggia@gmail.com>
2021-10-08 18:00:11 -0400

    gameWindow is now aware of the y position of the fish

--- src/gameWindow.js
+++ src/gameWindow.js
@@ -10,12 +10,25 @@ export default class gameWindow extends React.Component {
             yVelocity: 0,
             delta: 0.5
         };
+        this.fish= React.createRef();
+    }
+
+    getFishPos(){
+        console.log(this.fish.current.state.yPos)
+        return this.fish.current.state.yPos
+    }
+
+    componentDidMount(){
+        this.interval = setInterval(() => this.getFishPos(), 50);
+    }
+    componentWillUnmount(){
+        clearInterval(this.interval);
     }
 
     render() {
         return (
             <div id="GameWindow">
-                <Fish/>
+                <Fish ref={this.fish} />
             </div>
         );
     }
