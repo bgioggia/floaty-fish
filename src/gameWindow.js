c58b32f1fd4a6b76e11564fa24cfd126310b249d
bgioggia <bgioggia@gmail.com>
2021-10-08 20:41:54 -0400

    Added comments to gameWindow functions

--- src/gameWindow.js
+++ src/gameWindow.js
@@ -3,24 +3,31 @@ import Fish from './fish.js';
 import './gameWindow.css'
 export default class gameWindow extends React.Component {
 
+    // constructor for gameWindow
     constructor(props) {
         super(props);
         this.fish= React.createRef();
     }
 
-    getFishPos(){
+    // handleFishPosn updates the gameState
+    // - kills the fish if it touches a border of the gameWindow
+    handleFishPosn(){
         if(this.fish.current.state.yPos < -20 || this.fish.current.state.yPos > 623) {
             this.fish.current.die();
         }
     }
 
+    //mount
     componentDidMount(){
-        this.interval = setInterval(() => this.getFishPos(), 50);
+        this.interval = setInterval(() => this.handleFishPosn(), 50);
     }
+
+    //unmount
     componentWillUnmount(){
         clearInterval(this.interval);
     }
 
+    //Renders the game window
     render() {
         return (
             <div id="GameWindow">
